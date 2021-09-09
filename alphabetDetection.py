6ccd24a19cec752ea6543f9e809487977155e8e5
AbigailSayoj <69677006+AbigailSayoj@users.noreply.github.com>
2021-09-09 17:12:54 +0530

    Add files via upload

---
+++ alphabetDetection.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+# Fetching the data
+X = np.load("image.npz")['arr_0']
+y = pd.read_csv("label.csv")["labels"]
+print(pd.Series(y).value_counts())
+
+classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+nclasses = len(classes)
